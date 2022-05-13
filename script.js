efa98e5d61ba9bcb9008c1deff8ae63c108bc029
Dipesh Gupta <dipesh.gupta@w3villa.com>
2022-05-13 11:05:40 +0530

    Modified

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-
-hamburger.addEventListener("click",()=>{
-    navMenu.classList.toggle("active");
+//Creating toggle Navigation Bar
+document.querySelector(".hamburger").addEventListener("click", () => {
+  const navMenu = document.querySelector(".nav-menu");
+  navMenu.classList.toggle("active");
 })
 //Scroll Function to the window
 window.addEventListener("scroll", function () {
-    let head = document.querySelector(".nav-menu")       
-    let head2 = document.querySelector("header");
-    if (window.scrollY >= 150) {
-      head2.style.backgroundColor = "#3949ab";
-      head.style.backgroundColor = "#3949ab";
-      head2.style.padding = "0px";
-      head.style.top = "50px";
-      head2.style.transition = "0.3s";
-    }
-    else if (window.scrollY <= 150) {
-      head2.style.backgroundColor = "transparent";
-      head.style.backgroundColor = "transparent";
-      head.style.top = "70px";
-      head2.style.padding = "10px 0px";
-    }
-  })
-  // API Data
+  let head = document.querySelector(".nav-menu")
+  let head2 = document.querySelector("header");
+  if (window.scrollY >= 150) {
+    head2.style.backgroundColor = "#3949ab";
+    head.style.backgroundColor = "#3949ab";
+    head2.style.padding = "0px";
+    head.style.top = "50px";
+    head2.style.transition = "0.3s";
+  }
+  else if (window.scrollY <= 150) {
+    head2.style.backgroundColor = "transparent";
+    head.style.backgroundColor = "transparent";
+    head.style.top = "70px";
+    head2.style.padding = "10px 0px";
+  }
+})
+// Fetching API Data
 let fdata = fetch("https://reqres.in/api/users?page=2");
 fdata
   .then((res) => res.json())
@@ -49,4 +48,3 @@ fdata
       .join("");
     document.querySelector(".apidata").insertAdjacentHTML('afterbegin', html)
   });
-  
